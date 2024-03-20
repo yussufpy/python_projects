e10f12b38318a9a099ed11e7af0a1a5f3c39dada
Yussuf <yussuf.py@gmail.com>
2024-03-20 23:42:35 +0300

    initial commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your full name: ")
+age = int(input("Please " + name + " enter your age: "))
+location = input("Please " + name + " enter your location: ")
+print('\nPersonal Information\n')
+print(f'Hello {name}, you are {age} years old and you live in {location}')
